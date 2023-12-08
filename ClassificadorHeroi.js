0c1cabe9ae61dedd6e29ae987e3325b9d73f5e03
dacyn <dacy95s@gmail.com>
2023-12-07 21:32:09 -0300

    Projeto Classificador de nivel

---
+++ ClassificadorHeroi.js
@@ -0,0 +1,23 @@
+let nome = "Forgiven";
+let xp = 7005;
+let nivel;
+
+if (xp <= 1000) {
+    nivel = "Ferro";
+}else if(xp <= 2000) {
+    nivel = "Bronze";
+}else if(xp <= 5000) {
+    nivel = "Prata";
+}else if(xp <= 7000) {
+    nivel = "Ouro";
+}else if(xp <= 8000) {
+	nivel = "Platina Diamante";
+}else if(xp <= 9000) {
+	nivel = "Ascendente";
+}else if(xp <= 10000) {
+	nivel = "Imortal";
+}else {
+	nivel = "Radiante";
+}
+
+console.log("O Herói de nome " + nome + " está no nível -> " + nivel);
